84d4710ccc62f7d44875e9a29271bfa0bcc1be41
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 17:13:39 +0100

    Update classes.js

--- commands/classes.js
+++ commands/classes.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send( "__**Classes:**__\n **Type:** Math\n **Title:** Math Help\n please see <#449681498715455527> for more information")
+    message.channel.send( "__**Classes:**__\n \n **Type:** Math\n **Title:** Math Help\n \nplease see <#449681498715455527> for more information")
 }
