7a628d522cebb97e8591c64b85d5e9706be8262e
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 17:08:11 +0100

    Update classes.js

--- commands/classes.js
+++ commands/classes.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.author.send( "__**Classes:**__\n please see <#449681498715455527>")
+    message.channel.send( "__**Classes:**__\n please see <#449681498715455527>")
 }
