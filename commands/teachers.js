9944f5d3c60d9179396aa6879da9918460d987cf
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 18:20:12 +0100

    Update teachers.js

--- commands/teachers.js
+++ commands/teachers.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send( "__**Teachers:**__\n \n <@234837385186246666> <@341308015774793728> <@293817141537538048> \n \n please see <#450745292078186496> to apply to be a teacher.")
+    message.channel.send( "__**Teachers:**__\n \n <@234837385186246666> <@341308015774793728> <@293817141537538048> \n \n Please see <#450745292078186496> to apply to be a teacher.")
 }
