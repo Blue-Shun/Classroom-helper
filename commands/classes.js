9700f30c6ccb26930f7d0a5a9c47754e7b85fdb6
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 16:57:46 +0100

    Update classes.js

--- commands/classes.js
+++ commands/classes.js
@@ -1,16 +1,4 @@
 exports.run = (client, message, args) => {
-    message.author.send({embed: {
-	color: 15215,
-        author: {
-          name: "Classroom Helper",
-          icon_url: client.user.avatarURL
-        },
-        description: "__**Classes:**__\n please see <#449681498715455527>",
-        timestamp: new Date(),
-        footer: {
-          icon_url: message.author.avatarURL,
-          text: `${message.author.username} | Classroom Helper`
-        }
-      }
-    });
-}
+    message.author.send "__**Classes:**__\n please see <#449681498715455527>",
+});
+
