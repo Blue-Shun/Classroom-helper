44cfaef4d506178f28a27ba6585da4b044c9a6ff
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 18:24:31 +0100

    Create donate.js

---
+++ commands/donate.js
@@ -0,0 +1,3 @@
+		exports.run = (client, message, args) => {
+		message.channel.send("__**Donate**__\n As of right now, all services here are free! In order for us to continue offering quality education for free, we gladly encourage you to donate to our Patreon! You will receive a special role, and access to a special chat. More perks will be added soon!\nhttps://www.patreon.com/DiscordEducationalServer")	
+			}
