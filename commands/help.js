043541ff963ce30ddb48c8fe1688c1f9a3f6a051
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 18:02:10 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
 		exports.run = (client, message, args) => {
-		message.channel.send("__**Help**__\n**ping**\n**ban**\n**kick**")	
+		message.channel.send("__**Help**__\n**c!classes**\n**c!shedule**\n**c!teachers**\n**c!info**\n**c!donate**")	
 			}
 
